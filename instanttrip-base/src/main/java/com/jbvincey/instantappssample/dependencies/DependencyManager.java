4f0dab4e747e79182f30e91706c6e7ca55c09941
jean-baptiste.vincey <jean-baptiste.vincey@backelite.com>
2017-07-09 20:27:42 +0200

    feat (instanttrip-base) moving to instant app with one feature (base feature)

---
+++ instanttrip-base/src/main/java/com/jbvincey/instantappssample/dependencies/DependencyManager.java
@@ -0,0 +1,45 @@
+package com.jbvincey.instantappssample.dependencies;
+
+import android.content.Context;
+
+import com.jbvincey.instantappssample.presenters.DetailsPresenter;
+import com.jbvincey.instantappssample.presenters.DrawerPresenter;
+import com.jbvincey.instantappssample.presenters.MainPresenter;
+
+/**
+ * Created by jean-baptistevincey on 13/06/2017.
+ */
+
+public class DependencyManager {
+
+    private static DependencyManager instance;
+
+    private RepositoryModule repositoryModule;
+
+    private DependencyManager() {
+
+    }
+
+    public static DependencyManager getInstance() {
+        if(instance == null) {
+            instance = new DependencyManager();
+        }
+        return instance;
+    }
+
+    public void initWithContext(Context applicationContext) {
+        repositoryModule = RepositoryModule.getInstance(applicationContext);
+    }
+
+    public MainPresenter provideMainPresenter() {
+        return new MainPresenter(this.repositoryModule.provideTripRepository());
+    }
+
+    public DetailsPresenter provideDetailPresenter() {
+        return new DetailsPresenter(this.repositoryModule.provideTripRepository());
+    }
+
+    public DrawerPresenter provideDrawerPresenter() {
+        return new DrawerPresenter();
+    }
+}
---
+++ instanttrip-base/src/main/java/com/jbvincey/instantappssample/helper/IntentHelper.java
@@ -0,0 +1,83 @@
+package com.jbvincey.instantappssample.helper;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import com.jbvincey.instantappssample.BuildConfig;
+import com.jbvincey.instantappssample.constants.Constants;
+import com.jbvincey.instantappssample.model.Coordinates;
+
+/**
+ * Created by jean-baptistevincey on 24/06/2017.
+ */
+
+public final class IntentHelper {
+
+    public static final String INSTANT_TRIP_URL_AUTHORITY = "instantappsample.jbvincey.com";
+    public static final String KEY_QUERY_PARAMETER_TRIP = "tripId";
+    private static final String PLAYSTORE_APP_BASE_URL = "market://details?id=";
+    private static final String PLAYSTORE_BROWSER_BASE_URL = "https://play.google.com/store/apps/details?id=";
+    private static final String PACKAGE_MAPS = "com.google.android.apps.maps";
+    private static final String COORDINATES_PREFIX = "geo:0,0?q=";
+    private static final String COORDINATES_SEPARATOR = ",";
+    private static final String SHARE_INTENT_TYPE = "text/plain";
+    private static final String MAILTO_URI = "mailto:";
+    private static final String HTTPS_SCHEME = "https";
+    private static final String DETAILS_ACTIVITY_PATH = "trip-details";
+
+    public static Intent getMapsLocationItent(Coordinates coordinates) {
+        Uri mapsUri = Uri.parse(buildCoordinatesUri(coordinates));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapsUri);
+        mapIntent.setPackage(PACKAGE_MAPS);
+        return mapIntent;
+    }
+
+    private static String buildCoordinatesUri(Coordinates coordinates) {
+        return COORDINATES_PREFIX
+                + coordinates.getLatitude()
+                + COORDINATES_SEPARATOR
+                + coordinates.getLongitude();
+    }
+
+    public static Intent getContactIntent(String contact) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse(MAILTO_URI));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{contact});
+        return intent;
+    }
+
+    public static Intent getShareDetailUrlIntent(String tripId) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType(SHARE_INTENT_TYPE);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, buildDetailUrl(tripId));
+        return shareIntent;
+    }
+
+    private static String buildDetailUrl(String tripId) {
+        return Constants.BASE_URL + "/" + tripId;
+    }
+
+    public static Intent getInstantTripPlayStoreAppIntent() {
+        return new Intent(Intent.ACTION_VIEW, buildPlayStoreUrl(PLAYSTORE_APP_BASE_URL));
+    }
+
+    public static Intent getInstantTripPlayStoreBrowserIntent() {
+        return new Intent(Intent.ACTION_VIEW, buildPlayStoreUrl(PLAYSTORE_BROWSER_BASE_URL));
+    }
+
+    private static Uri buildPlayStoreUrl(String baseUrl) {
+        return Uri.parse(baseUrl + BuildConfig.APPLICATION_ID);
+    }
+
+    public static Intent getDetailsActivityUrl(String tripId) {
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder
+                .scheme(HTTPS_SCHEME)
+                .authority(INSTANT_TRIP_URL_AUTHORITY)
+                .appendPath(DETAILS_ACTIVITY_PATH)
+                .appendQueryParameter(KEY_QUERY_PARAMETER_TRIP, tripId);
+
+        return new Intent(Intent.ACTION_VIEW, uriBuilder.build());
+    }
+
+}
